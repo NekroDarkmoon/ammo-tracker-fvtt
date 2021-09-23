535f4a82c0cadb3b04cae7eec9a991a3a3ad1f77
NekroDarkmoon <junaid_maclaren97@hotmail.com>
2021-09-23 05:49:21 -0700

    Working prototype for GMs

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 import { moduleName, moduleTag, messageDelete } from "./scripts/constants.js";
 import { registerSettings } from "./scripts/settings.js";
-import {Tracker} from "./scripts/tracker.js"
+import {AmmoTracker} from "./scripts/tracker.js"
 
-// export let socket;
+export let socket;
+let trackers = [];
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                              Main
@@ -17,13 +18,73 @@ Hooks.on('init', async () => {
 
 
 Hooks.on('socketlib.ready', () => {
-    // socket = socketlib.registerModule(moduleName);
-    // socket.register("messageDelete", messageDelete);
+    socket = socketlib.registerModule(moduleName);
+    socket.register("messageDelete", messageDelete);
 });
 
 
 Hooks.on('ready', async () => {    
-   console.log(`${moduleTag} | Ready`);
-
+    console.log(`${moduleTag} | Ready`);
+    watcher();
 });
 
+
+Hooks.on('createCombat', async (...args) => {
+    const tracker = new AmmoTracker(args[0].data._id);
+    trackers.push(tracker);
+});
+
+
+Hooks.on('updateCombat', async (...args) => {
+    if (args[0].data.round === 0) { return true; }
+    
+    for (let tracker of trackers) {
+        if (tracker.combatId == args[0].data._id){
+            if (!tracker.started) {
+                tracker.started = true;
+                await tracker.startTracker();
+            }
+            break;
+        }
+    }
+}); 
+
+
+Hooks.on('deleteCombat', async (...args) => {
+    for (let tracker of trackers) {
+        if (tracker.combatId == args[0].data._id) {
+            if (tracker.started) {
+                tracker.ended = true;
+                await tracker.endTracker();
+            }
+        }
+    }
+});
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                            Watcher
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function watcher() {
+    $(document).on('click', '.at-recover-btn', async (button) => {
+        if (!game.user.isGM) {
+            
+        }
+
+        let currentTracker = trackers.find(tracker => tracker.combatId == button.currentTarget.dataset.combatId );
+        console.log(currentTracker);
+        
+        if (currentTracker != undefined){
+            let dataset = button.currentTarget.dataset; 
+            await currentTracker.recover(dataset.actorId);
+        }
+
+    });
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                            Tracker
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                            Tracker
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- scripts/tracker.js
+++ scripts/tracker.js
@@ -6,5 +6,183 @@ import {socket} from "../index.js";
 
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//                            Person
+//                          Ammo Tracker
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+export class AmmoTracker {
+
+    constructor(combatId) {
+        this.combatId = combatId;
+        this.started = false;
+        this.ended = false;
+
+        this.actorIds = null;
+        this.combat = game.combats.get(combatId);
+    }
+
+    async startTracker() {
+        let currCombat = game.combats.get(this.combatId);
+        this.actorIds = this.fetchActorIds(currCombat);
+        const actors = this.fetchActors();
+        
+        // Get projectile data for all actors
+        const projectileData = await this.getProjectilesData(actors);
+        await currCombat.setFlag(moduleName, 'projectileData', projectileData);
+
+    }
+
+
+    async endTracker() {
+        // Get used ammor for each actor
+        const actors = this.fetchActors();
+        const sentMsgs = {};
+        for (let actor of actors) {
+            const usedAmmo = this.usedAmmo(actor);
+            // Skip if no ammo consumed
+
+            sentMsgs[actor.data._id] = await this.toMessage(actor, usedAmmo);
+        }
+
+        // Set setting for deletion
+        await game.settings.set(moduleName, 'chat-trackers', sentMsgs);
+
+    }
+
+
+    async getProjectilesData(actors) {
+        // Use actor ids as keys
+        const projectileData = {}
+
+        for (let actor of actors) {
+            let projectiles = this.fetchProjectileItems(actor);
+            
+            // Use item ids as keys
+            const data = {};
+            for (let item of projectiles) {
+                data[item._id] = item.data.quantity;
+            }
+
+            // Add item flag to actor
+            actor.setFlag(moduleName, 'projectileData', data);
+            projectileData[actor.data._id] = data;
+        }
+        return projectileData;
+    }
+
+
+    usedAmmo(actor) {
+        const projectileItems = this.fetchProjectileItems(actor);
+        const projectileData = this.combat.getFlag(moduleName, 'projectileData');
+
+        let data = [];
+
+        for (let item of projectileItems) {
+            const startAmt = (projectileData[actor.data._id])[item._id];
+            const endAmt = item.data.quantity;
+
+            if (endAmt !== startAmt) {
+                const ammoData = this.calc(startAmt, endAmt);
+                data.push( {item, ammoData} );
+            }
+        }
+
+        return data;
+    }
+
+
+    async recover(actorId) {
+        // Vars
+        let actor = game.actors.get(actorId);
+        const data = this.usedAmmo(actor);
+        let message = "";
+        let updates = [];
+        console.log(game.settings.get(moduleName, 'chat-trackers'));
+        const messageId = game.settings.get(
+            moduleName, 'chat-trackers')[actorId];
+                
+        // Delete previous message.
+        await game.messages.get(messageId).delete();
+
+        // Send new message to group
+        for (let elem of data) {
+            const ammoData = elem.ammoData;
+            const item = elem.item;
+            const newCount = ammoData.endAmt + ammoData.recoverable;
+            updates.push( {_id: item._id, "data.quantity": newCount} );
+            
+            message += `${elem.item.name}: ${ammoData.startAmt} ➔ ${ammoData.endAmt}`;
+            message += `<br><b>Consumed:</b> ${ammoData.consumed}`;
+            message += `<br><b>Recovered:</b> ${ammoData.recoverable}<hr>`;
+        }
+
+        actor.updateEmbeddedDocuments("Item", updates);
+        console.info(`${moduleTag} | Updated item counts.`);
+        
+        let button = `<button data-actor-id="${actor.data._id}"
+                        class="at-recovered-btn disabled">Recovered!</button>`; 
+        
+        await ChatMessage.create({
+            content: [message, button].join(''),
+            speaker: ({alias: `${actor.data.name}`}), 
+        });
+    }
+
+
+    calc(startAmt, endAmt) {
+        const PERCENT = 50;
+
+        const consumed = startAmt - endAmt;
+        const recoverable = Math.ceil(consumed * (PERCENT * 0.01));
+
+        // return {consumed: consumed, endAmt: endAmt, startAmt: startAmt, recoverable: recoverable};
+        return { consumed, endAmt, startAmt, recoverable};
+    }
+
+    fetchActorIds(combat) {
+        return combat.data.combatants._source.map(actor => actor.actorId);
+    }
+
+    fetchActors() {
+        return this.actorIds.map(actorId => game.actors.get(actorId));
+    }
+
+    fetchProjectileItems(actor) {
+        return actor.data.items._source.filter(item => item.data.consumableType == 'ammo');
+    }
+
+    async toMessage(actor, data) {
+        // Send to chat
+        let message = "";
+        for (let elem of data) {
+            const ammoData = elem.ammoData;
+            message += `${elem.item.name}: ${ammoData.startAmt} ➔ ${ammoData.endAmt}`;
+            message += `<br><b>Consumed:</b> ${ammoData.consumed}`;
+            message += `<br><b>Recoverable:</b> ${ammoData.recoverable}<hr>`;
+        }
+
+        let button = `<button data-actor-id="${actor.data._id}" 
+                        data-combat-id="${this.combatId}"
+                        class="at-recover-btn">Recover Items</button>`;
+
+        let chat = await ChatMessage.create({
+            content: [message, button].join(''),
+            speaker: ({alias: `${actor.data.name}`}), 
+            whisper: ChatMessage.getWhisperRecipients(actor.data.name)
+        });
+
+
+        return chat.data._id;
+
+    }
+
+}
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                            Tracker
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                            Tracker
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                            Tracker
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
