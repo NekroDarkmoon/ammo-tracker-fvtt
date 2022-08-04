faa8ec5d92e3f04efc765eda363724342b60cbf2
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-08-03 18:57:13 -0700

    Added base AmmoTracker Class

--- scripts/AmmoTracker.js
+++ scripts/AmmoTracker.js
@@ -29,4 +29,82 @@ export class AmmoTracker {
 		const projectileData = await this.getProjectileData(actors);
 		await currCombat.setFlag(moduleName, 'projectileData', projectileData);
 	}
+
+	/**
+	 * Ends Ammunition tracking for a given tracker.
+	 */
+	async endTracker() {
+		// Get used ammo for each actor
+		const actors = this.fetchActors();
+		const sentMsgs = {};
+		for (const actor of actors) {
+			const usedAmmo = this.usedAmmo(actor);
+			// Skip if no ammo consumed
+			if (usedAmmo.length == 0) continue;
+
+			sentMsgs[actor.id] = await this.toMessage(actor, usedAmmo);
+		}
+
+		// Set setting for deletion
+		await game.settings.set(moduleName, 'chat-trackers', sentMsgs);
+	}
+
+	/**
+	 *
+	 * @param {*} combat
+	 * @returns
+	 */
+	fetchActorIds(combat) {
+		return combat.combatants.map(a => a.actorId);
+	}
+
+	fetchActors() {
+		const actors = this.actorIds.map(id => game.actors.get(id));
+		return actors.filters(a => a.type === 'character');
+	}
+
+	/**
+	 *
+	 * @param {*} actor
+	 * @param {*} data
+	 * @returns
+	 */
+	async toMessage(actor, data) {
+		// Send to chat
+		let msg = '';
+		for (const elem of data) {
+			const ammoData = elem.ammoData;
+			msg += `${elem.item.name}: ${ammoData.startAmt} ➔ ${ammoData.endAmt}`;
+			msg += `<br><b>Consumed:</b> ${ammoData.consumed}`;
+			msg += `<br><b>Recoverable:</b> ${ammoData.recoverable}<hr>`;
+		}
+
+		const button = `<button data-actor-id="${actor._id}" 
+                        data-combat-id="${this.combatId}"
+                        class="at-recover-btn">Recover Items</button>`;
+
+		const chat = await ChatMessage.create({
+			content: [message, button].join(''),
+			speaker: { alias: `${actor.name}` },
+			whisper: ChatMessage.getWhisperRecipients(actor.name),
+		});
+
+		return chat._id;
+	}
+
+	async recoverMsg(actorId) {}
+
+	// ***************************************
+	// Overrides
+	// ***************************************
+	/**
+	 *
+	 * @param {*} actors
+	 * @returns
+	 */
+	async getProjectileData(actors) {}
+
+	usedAmmo(actor) {}
+
+	async recover(actorId) {}
 }
