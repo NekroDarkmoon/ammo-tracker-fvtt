886e70a08f3785635daeb940834a359381dce667
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-10-27 07:08:54 -0700

    Todo

--- scripts/systems/dnd5e.js
+++ scripts/systems/dnd5e.js
@@ -10,6 +10,7 @@ import { moduleName, moduleTag } from '../constants.js';
 export class Dnd5eTracker extends AmmoTracker {
 	constructor(combatId, resumed = false) {
 		super(combatId, resumed);
+		// TODO: Move out to main
 		this.magicConsumed = game.settings.get(moduleName, 'trackMagic');
 	}
 
